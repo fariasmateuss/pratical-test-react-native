0f0115a474c6e29f01be517ace4b77403918ca20
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-08-26 04:06:06 -0400

    feat: add api payload schema

---
+++ src/shared/types/apiSchema.ts
@@ -0,0 +1,15 @@
+export type SearchBook = {
+  author: string;
+  title: string;
+};
+
+export type Book = {
+  book_image: string;
+  author: string;
+  title: string;
+};
+
+export type Category = {
+  display_name: string;
+  list_name_encoded: string;
+};
